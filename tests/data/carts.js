e21df50317ff7dd4a7be54dc5825dcd8b677f167
dridrione <aauxent@ineat.fr>
2021-04-23 11:07:36 +0200

    small changes while havin fun with webpack

---
+++ tests/data/carts.js
@@ -0,0 +1,40 @@
+export let cartWith10Quantity = `{
+    "5beaa8bf1c9d440000a57d94-Darkbrown": {
+        "colors": [
+            "Tan",
+            "Chocolate",
+            "Black",
+            "White"
+        ],
+        "_id": "5be9c8541c9d440000665243",
+        "name": "Norbert",
+        "price": 2900,
+        "imageUrl": "http://localhost:3000/images/teddy_1.jpg",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        "quantity": 3
+    },
+    "5beaa8bf1c9d440000a57d94-Palebrown": {
+        "colors": [
+            "Pale brown",
+            "Dark brown",
+            "White"
+        ],
+        "_id": "5beaa8bf1c9d440000a57d94",
+        "name": "Arnold",
+        "price": 3900,
+        "imageUrl": "http://localhost:3000/images/teddy_2.jpg",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        "quantity": 3
+    },
+    "5be9c8541c9d440000665243-Tan": {
+        "colors": [
+            "Brown"
+        ],
+        "_id": "5beaaa8f1c9d440000a57d95",
+        "name": "Lenny and Carl",
+        "price": 5900,
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        "imageUrl": "http://localhost:3000/images/teddy_3.jpg",
+        "quantity": 4
+    }
+}`
--- tests/services/cart-service-spec.js
+++ tests/services/cart-service-spec.js
@@ -1,4 +1,5 @@
 import { stringWithoutSpace, getCartSize } from "../../src/js/services/CartService.js";
+import { cartWith10Quantity } from "../data/carts.js"
 
 
 describe('stringWithoutSpace', () => {
@@ -15,58 +16,14 @@ describe('stringWithoutSpace', () => {
 });
 
 describe('getCartSize', () => {
-    // grabs #cart-counter from the DOM
-    // gets cart from local storage
-    // if there is a cart, updates the number
-        //
     it('should return a number', () => {
         // arrange
-        let cart = `{
-            "5beaa8bf1c9d440000a57d94-Darkbrown": {
-                "colors": [
-                    "Tan",
-                    "Chocolate",
-                    "Black",
-                    "White"
-                ],
-                "_id": "5be9c8541c9d440000665243",
-                "name": "Norbert",
-                "price": 2900,
-                "imageUrl": "http://localhost:3000/images/teddy_1.jpg",
-                "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-                "quantity": 3
-            },
-            "5beaa8bf1c9d440000a57d94-Palebrown": {
-                "colors": [
-                    "Pale brown",
-                    "Dark brown",
-                    "White"
-                ],
-                "_id": "5beaa8bf1c9d440000a57d94",
-                "name": "Arnold",
-                "price": 3900,
-                "imageUrl": "http://localhost:3000/images/teddy_2.jpg",
-                "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-                "quantity": 3
-            },
-            "5be9c8541c9d440000665243-Tan": {
-                "colors": [
-                    "Brown"
-                ],
-                "_id": "5beaaa8f1c9d440000a57d95",
-                "name": "Lenny and Carl",
-                "price": 5900,
-                "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-                "imageUrl": "http://localhost:3000/images/teddy_3.jpg",
-                "quantity": 4
-            }
-        }`
+        let cart = cartWith10Quantity;
         let parsedCart = JSON.parse(cart)
         // act 
         // assert
         expect(getCartSize(parsedCart)).toBe(10);
     })
-
     it('should return 0 if cart null', () => {
         // arrange
         let cart = null;
@@ -74,7 +31,6 @@ describe('getCartSize', () => {
         // assert
         expect(getCartSize(cart)).toBe(0);
     })
-    
     it('should return 0 if cart ""', () => {
         // arrange
         let cart = "";
@@ -82,5 +38,4 @@ describe('getCartSize', () => {
         // assert
         expect(getCartSize(cart)).toBe(0);
     })
-
 })
