d460d556ca811fb9f610825f790eb0091a81454f
Adrien Auxent <a.auxent@gmail.com>
2021-04-26 20:03:02 +0200

    gitignore

---
+++ dist/assets/article.99d532e8.js
@@ -0,0 +1,1 @@
+import{u as a}from"./CartService.ddf7958b.js";import{a as e}from"./TeddiesService.3f7a9ff4.js";e(new URLSearchParams(window.location.search).get("articleId")),a();
---
+++ dist/assets/main.8b4ed017.js
@@ -0,0 +1,1 @@
+import{u as r}from"./CartService.ddf7958b.js";import{g as e}from"./TeddiesService.3f7a9ff4.js";e("#bears-list"),r();
