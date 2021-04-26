83be81d5a5c9df1f32147ccad4525c8cec4f54e7
Adrien Auxent <a.auxent@gmail.com>
2021-04-26 20:03:48 +0200

    dist folder remove

--- dist/assets/article.99d532e8.js
+++
@@ -1,1 +0,0 @@
-import{u as a}from"./CartService.ddf7958b.js";import{a as e}from"./TeddiesService.3f7a9ff4.js";e(new URLSearchParams(window.location.search).get("articleId")),a();
--- dist/assets/main.8b4ed017.js
+++
@@ -1,1 +0,0 @@
-import{u as r}from"./CartService.ddf7958b.js";import{g as e}from"./TeddiesService.3f7a9ff4.js";e("#bears-list"),r();
