4b2cdf5ea7068f8841238afe4c1a49e6ad3de3ce
dridrione <aauxent@ineat.fr>
2021-05-05 19:20:31 +0200

    background table panier

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,6 @@
 import '../scss/style.scss'
-import {getTeddiesListIntoContainer, createCardList} from "./services/TeddiesService.js"
+import {getTeddiesListIntoContainer} from "./services/TeddiesService.js"
 import {updateCartCounter} from "./services/CartService.js"
-import {teddiesResponse } from "../../tests/data/teddiesResponse"
 
-
+getTeddiesListIntoContainer("#bears-list");
 updateCartCounter();
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const environment = urlParams.get('environment')
-if (environment === "test") {
-    createCardList("#bears-list", teddiesResponse);} else {
-        getTeddiesListIntoContainer("#bears-list");
-    }
